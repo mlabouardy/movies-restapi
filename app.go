a75b9b76302cd9537774ca4e3ad3c28a00b4bca9
mlabouardy <mohamed@labouardy.com>
2017-08-17 13:34:24 +0200

    refactoring

--- app.go
+++ app.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
 	"gopkg.in/mgo.v2/bson"
 
+	. "./config"
+	. "./dao"
+	. "./models"
 	"github.com/gorilla/mux"
-	. "github.com/mlabouardy/movies-restapi/config"
-	. "github.com/mlabouardy/movies-restapi/dao"
-	. "github.com/mlabouardy/movies-restapi/models"
 )
 
 var config = Config{}
 var dao = MoviesDAO{}
 
 func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
-	var movies = dao.FindAll()
-	json.NewEncoder(w).Encode(&movies)
+	movies, err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, movies)
 }
 
 func FindMovieEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	var movie = dao.FindById(params["id"])
-	json.NewEncoder(w).Encode(&movie)
+	movie, err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, movie)
 }
 
 func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -33,20 +40,42 @@ func CreateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
 	var movie Movie
 	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
 	}
 	movie.ID = bson.NewObjectId()
 	if err := dao.Insert(movie); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 	respondWithJson(w, http.StatusCreated, movie)
 }
 
 func UpdateMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Update(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func DeleteMovieEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	defer r.Body.Close()
+	var movie Movie
+	if err := json.NewDecoder(r.Body).Decode(&movie); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if err := dao.Delete(movie); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
@@ -74,7 +103,7 @@ func main() {
 	r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
 	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
 	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")
-	r.HandleFunc("/movies/:id", FindMovieEndpoint).Methods("GET")
+	r.HandleFunc("/movies/{id}", FindMovieEndpoint).Methods("GET")
 	if err := http.ListenAndServe(":3000", r); err != nil {
 		log.Fatal(err)
 	}
--- dao/movies_dao.go
+++ dao/movies_dao.go
@@ -3,7 +3,7 @@ package dao
 import (
 	"log"
 
-	. "github.com/mlabouardy/movies-restapi/models"
+	. "../models"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
